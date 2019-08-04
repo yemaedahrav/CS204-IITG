1cd17f4fad4e5f21641a461b6ef6343bcf843236
yemaedahrav <50958687+yemaedahrav@users.noreply.github.com>
2019-08-05 03:15:51 +0530

    Update Addition.cpp

--- Addition.cpp
+++ Addition.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
 
+using namespace std;
+
+int main()
+{ long long int n,sqr;
+  while(1)
+  {cout<<"Enter the number to be checked \n";
+   cin>>n;
+   if(n==0) break;
+   sqr=sqrtl(n);
+  
+  int i,k;
+  k=0;
+  for(i=2;i<=sqr;i++)
+  { if(n%i==0) 
+    {cout<<"The number is not prime \n";
+    k+=1;
+    break;
+    }
+  }
+  
+
+if(k==0) cout<<"The number is prime \n";
+ }
+     
+
+return 0;
+}
+    
