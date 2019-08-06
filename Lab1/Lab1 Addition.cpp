4bc13e208924f8f62823686404859cfa0995eb12
yemaedahrav <50958687+yemaedahrav@users.noreply.github.com>
2019-08-06 18:47:08 +0530

    Update Lab1 Addition.cpp

--- Lab1/Lab1 Addition.cpp
+++ Lab1/Lab1 Addition.cpp
@@ -13,9 +13,11 @@ while(count--)
 {
 string a,b,sum;
 char carry;
+    
 cin>>a>>b;
 reverse(a.begin(),a.end());
 reverse(b.begin(),b.end());
+    
 int x,y;
 x=max(a.length(),b.length());
 y=min(a.length(),b.length());
@@ -24,18 +26,24 @@ int p,q;
 int rem=0;
 for(int i=0;i<x;i++){
     if(i<y){
+        
         p=a[i]-'0';
         q=b[i]-'0';
         carry=('0'+((p+q+rem)%10));
+        
         sum=sum+carry;
         rem=(p+q+rem)/10;
+        
     }else{
+        
         if(a.length()==x){
             p=a[i]-'0';
         }else{
             p=b[i]-'0';
         }
+        
         carrry=('0'+((p+rem)%10));
+        
         sum=sum+carry;
         rem=(p+rem)/10;
     }
@@ -43,7 +51,10 @@ for(int i=0;i<x;i++){
 if(rem==1){
     sum=sum+'1';
 }
+    
 reverse(sum.begin(),sum.end());
+    
+    
 cout<<sum<<endl;
 }
     
