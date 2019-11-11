e126cf0ed6599a57f427f3a7905cb0fc7287353a
yemaedahrav <50958687+yemaedahrav@users.noreply.github.com>
2019-11-11 16:56:14 +0530

    Create withouthashinh.cpp

---
+++ Lab13/withouthashinh.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string reverse(string word)
+       {  
+          string revword = "";
+          int n=word.length();
+          for(int i=n-1;i>=0;i--)
+             {  revword.push_back(word[i]); }             }
+       
+          return revword;
+     
+       }
+
+int main()
+{ 
+  long long int n;
+  cin>>n;
+
+  while(n--)
+   {  
+      string word;
+      cin>>word;
+      
+      if(word!=revword)
+        {  
+
+
+        }
+   }
+
+return 0;
+
+}
