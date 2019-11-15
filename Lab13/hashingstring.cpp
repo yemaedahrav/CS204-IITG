d55f7cf03b0c26c4df343fa303156dc32835591a
yemaedahrav <50958687+yemaedahrav@users.noreply.github.com>
2019-11-15 06:09:14 +0530

    Create hashingstring.cpp

---
+++ Lab13/hashingstring.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long
+#define fori(n) for(lli i=0;i<n;i++)
+const int p=53;
+const int m=1e7+19;
+
+lli hash_func(string const& s) {
+    
+    
+    long long hash_value = 0;
+    long long p_pow = 1;
+    for (char c : s) {
+        hash_value = (hash_value + (c - 'A' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+    }
+    return hash_value;
+}
+int main(){
+	int n;
+	cin>>n;
+	vector<string> inps;
+	
+	vector<vector<string>> hashTable(m);
+
+
+	string temp,revstr;
+	
+
+	for(int i=0;i<n;i++){
+		cin>>temp;
+		revstr=temp;
+		reverse(revstr.begin(),revstr.end());
+		
+		if(temp!=revstr){
+			hashTable[hash_func(temp)].push_back(temp);
+			inps.push_back(temp);
+		}
+
+			
+	}
+	bool final=false;
+	for(int i=0;i<inps.size();i++){
+		temp=inps[i];
+		revstr=temp;
+		reverse(revstr.begin(),revstr.end());
+		
+
+		if(hashTable[hash_func(revstr)].size()!=0){
+			for(int i=0;i<hashTable[hash_func(revstr)].size();i++){
+				if(hashTable[hash_func(revstr)][i]==revstr){
+					final=true;
+					break;
+				}
+			}
+
+		}
+
+
+	}
+
+	if(final) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+}
